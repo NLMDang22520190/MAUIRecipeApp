bb4671dad0c64e7bd0c7734d7765ce493286c90c
Tekatoji <22520190@gm.uit.edu.vn>
2024-09-13 20:49:51 +0700

    Add FontAwesome icons and update UI elements
    Updated `MAUIRecipeApp.csproj` to include `UraniumUI.Icons.FontAwesome` package version `2.9.1`. Modified `MauiProgram.cs` to add FontAwesome icon fonts. Enhanced `LoginPageView.xaml` with new UI elements and FontAwesome icons for Google and Facebook login buttons. Adjusted layout properties in `SignUpPageView.xaml` and added a new `material:TextField` for "Username" with an icon.

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -25,6 +25,7 @@ namespace MAUIRecipeApp
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                     fonts.AddMaterialSymbolsFonts();
+                    fonts.AddFontAwesomeIconFonts();
                 });
 
 #if DEBUG
