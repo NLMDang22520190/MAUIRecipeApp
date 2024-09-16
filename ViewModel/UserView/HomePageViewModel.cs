6568ecd086f6764e0e8142f4fed816d41ebe9ba4
Tekatoji <22520190@gm.uit.edu.vn>
2024-09-16 16:40:50 +0700

    Refactor HomePage and update ViewModel for CollectionView
    - Removed Command attribute from Google sign-in button in LoginPageView.xaml.
    - Added namespaces to HomePageView.xaml: UraniumUI.Icons.FontAwesome, UraniumUI.Icons.MaterialSymbols, and MAUIRecipeApp.Models.
    - Replaced HorizontalStackLayout with CollectionView for food recipe types in HomePageView.xaml.
    - Replaced FlexLayout with CollectionView for food recipes in HomePageView.xaml.
    - Updated HomePageViewModel.cs to include observable collections: FoodRecipeTypes and FoodRecipes.
    - Added constructor to HomePageViewModel to initialize collections via LoadItem method.
    - Implemented LoadItem method to filter and load non-deleted items from the database.
    - Added using directives to HomePageViewModel.cs for MAUIRecipeApp.Models, Microsoft.EntityFrameworkCore, and System.Collections.ObjectModel.

--- ViewModel/UserView/HomePageViewModel.cs
+++ ViewModel/UserView/HomePageViewModel.cs
@@ -1,19 +1,49 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using MAUIRecipeApp.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MAUIRecipeApp.ViewModel.UserView
 {
-    public partial class HomePageViewModel: ObservableObject
+    public partial class HomePageViewModel : ObservableObject
     {
+        [ObservableProperty]
+        ObservableCollection<FoodRecipeType> foodRecipeTypes;
+
+        [ObservableProperty]
+        ObservableCollection<FoodRecipe> foodRecipes;
+
+        public HomePageViewModel()
+        {
+            LoadItem();
+        }
+
+
+
+
         [RelayCommand]
         public async Task FoodDetail()
         {
             await Shell.Current.GoToAsync("//fooddetail");
+        }
+
+        private void LoadItem()
+        {
+            // Lọc các phần tử không bị xóa trong FoodRecipeTypes
+            FoodRecipeTypes = new ObservableCollection<FoodRecipeType>(
+                DataProvider.Ins.DB.FoodRecipeTypes.AsNoTracking()
+                .Where(item => (bool)!item.IsDeleted).ToList());
+
+            // Lọc các phần tử không bị xóa trong FoodRecipes
+            FoodRecipes = new ObservableCollection<FoodRecipe>(
+                DataProvider.Ins.DB.FoodRecipes.AsNoTracking()
+                .Where(item => (bool)!item.IsDeleted).ToList());
         }
     }
 }
